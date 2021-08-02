56567c34786f3c8d402f7e83a91cbc4662e0049a
Jessie Panganiban <jessiepanganiban@gmail.com>
2021-08-02 17:13:37 +0200

    push

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv/config");
+require("./config/db");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require("express-session");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const User = require("./models/User");
+const MongoStore = require("connect-mongo");
 
-var app = express();
+const app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +29,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signin', authRouter);
+app.use('/signout', authRouter);
+app.use('/signout', authRouter);
+
+//app.use(
+//    session({
+//      secret: process.env.SESSION_SECRET,
+//      resave: true,
+//      saveUnitialized: true,
+//      store: MongoStore.create({ mongoURL: process.env.MONGO_URI }),
+//      cookie: { secure: true },
+//    })
+//);
+
+app.use(require("./middlewares/auth")); 
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const requireAuth = require("../middlewares/auth");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
